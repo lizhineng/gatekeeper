6120c1b2bc62f520b8caeeff14bb4b14ecc55b47
Li Zhineng <im@zhineng.li>
2023-05-14 15:31:42 +0800

    rename class

--- tests/RoleFilterTest.php
+++ tests/RoleFilterTest.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Collection;
 use Zhineng\Gatekeeper\Models\Role;
 use Zhineng\Gatekeeper\Tests\Fixtures\User;
 
-class RoleScopeTest extends FeatureTest
+class RoleFilterTest extends FeatureTest
 {
     public function test_scopes_users_by_role()
     {
