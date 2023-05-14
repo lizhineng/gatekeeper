a1725e8b1996ab6289a90465b55a7250467ac0e5
Li Zhineng <im@zhineng.li>
2023-05-14 15:27:25 +0800

    remove normalize

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -11,18 +11,6 @@ class Permission extends Model
 
     protected $guarded = [];
 
-    public static function normalize(Permission|iterable|string $permissions)
-    {
-        if (is_iterable($permissions)) {
-            return collect($permissions)
-                ->map(fn ($permission) => static::$gatekeeper->permission($permission)->getKey());
-        } elseif (is_string($permissions)) {
-            return static::$gatekeeper->permission($permissions);
-        }
-
-        return $permissions;
-    }
-
     public function roles()
     {
         return $this->morphedByMany(Role::class, 'assignable', 'assigned_permissions');
