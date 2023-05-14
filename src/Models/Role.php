f73dfcca30cb2cccaa74c7798e28732b13be739d
Li Zhineng <im@zhineng.li>
2023-05-14 15:01:40 +0800

    improve role

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -3,41 +3,11 @@
 namespace Zhineng\Gatekeeper\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Zhineng\Gatekeeper\Concerns\HasPermissions;
 
 class Role extends Model
 {
+    use HasPermissions;
+
     protected $guarded = [];
-
-    public function permissions()
-    {
-        return $this->morphToMany(Permission::class, 'assignable', 'assigned_permissions');
-    }
-
-    public function assignPermission(Permission|iterable|string $permissions): self
-    {
-        $permissions = Permission::normalize($permissions);
-
-        $this->permissions()->attach($permissions);
-
-        return $this;
-    }
-
-    public function removePermission(Permission|iterable|string $permissions): self
-    {
-        $permissions = Permission::normalize($permissions);
-
-        $this->permissions()->detach($permissions);
-
-        return $this;
-    }
-
-    public function allows(Permission $permission): bool
-    {
-        return $this->permissions->contains($permission);
-    }
-
-    public function allowsAny(array $permissions): bool
-    {
-        return collect($permissions)->some(fn ($permission) => $this->allows($permission));
-    }
 }
--- tests/RoleTest.php
+++ tests/RoleTest.php
@@ -7,74 +7,52 @@ use Zhineng\Gatekeeper\Models\Role;
 
 class RoleTest extends FeatureTest
 {
-    public function test_assigns_permission_to_role()
+    use ProvidesPermissions;
+
+    /**
+     * @dataProvider provides_permissions
+     */
+    public function test_assigns_permission_to_role($getPermissions)
     {
-        $readPosts = Permission::create(['name' => 'read:posts']);
+        $permissions = $getPermissions();
 
         $editor = Role::create(['name' => 'editor']);
-        $this->assertFalse($editor->allows($readPosts));
+        $this->assertFalse($editor->allows($permissions));
 
-        $editor->assignPermission($readPosts);
-        $this->assertTrue($editor->fresh()->allows($readPosts));
+        $editor->assignPermission($permissions);
+        $this->assertTrue($editor->fresh()->allows($permissions));
     }
 
-    public function test_assigns_permission_to_role_by_scope()
+    /**
+     * @dataProvider provides_permissions
+     */
+    public function test_removes_permission_from_role($getPermissions)
     {
-        $readPosts = Permission::create(['name' => 'read:posts']);
+        $permissions = $getPermissions();
 
-        $editor = Role::create(['name' => 'editor']);
-        $this->assertFalse($editor->allows($readPosts));
+        $editor = Role::create(['name' => 'editor'])->assignPermission($permissions);
+        $this->assertTrue($editor->allows($permissions));
 
-        $editor->assignPermission($readPosts->name);
-        $this->assertTrue($editor->fresh()->allows($readPosts));
+        $editor->removePermission($permissions);
+        $this->assertFalse($editor->fresh()->allows($permissions));
     }
 
-    public function test_assigns_multiple_permissions_to_role()
-    {
-        $readPosts = Permission::create(['name' => 'read:posts']);
-        $writePosts = Permission::create(['name' => 'write:posts']);
-
-        $editor = Role::create(['name' => 'editor']);
-        $editor->assignPermission([$readPosts, $writePosts->name]);
-        $this->assertTrue($editor->fresh()->allows($readPosts));
-        $this->assertTrue($editor->fresh()->allows($writePosts));
-    }
-
-    public function test_removes_permission_from_role()
-    {
-        $readPosts = Permission::create(['name' => 'read:posts']);
-        $editor = Role::create(['name' => 'editor']);
-
-        $editor->assignPermission($readPosts);
-        $editor->removePermission($readPosts);
-
-        $this->assertFalse($editor->allows($readPosts));
-    }
-
-    public function test_removes_permission_from_role_by_scope()
-    {
-        $readPosts = Permission::create(['name' => 'read:posts']);
-        $editor = Role::create(['name' => 'editor']);
-
-        $editor->assignPermission($readPosts);
-        $editor->removePermission($readPosts->name);
-
-        $this->assertFalse($editor->allows($readPosts));
-    }
-
-    public function test_removes_multiple_permissions_from_role()
+    public function test_allows_all_permissions_checking()
     {
         $readPosts = Permission::create(['name' => 'read:posts']);
         $writePosts = Permission::create(['name' => 'write:posts']);
 
         $editor = Role::create(['name' => 'editor']);
-        $editor->assignPermission([$readPosts, $writePosts]);
-        $editor->removePermission([$readPosts, $writePosts->name]);
+        $this->assertFalse($editor->allowsAll([$readPosts, $writePosts]));
 
-        $this->assertFalse($editor->allowsAny([$readPosts, $writePosts]));
+        $editor->assignPermission($readPosts);
+        $this->assertFalse($editor->fresh()->allowsAll([$readPosts, $writePosts]));
+
+        $editor->assignPermission($writePosts);
+        $this->assertTrue($editor->fresh()->allowsAll([$readPosts, $writePosts]));
     }
 
-    public function test_checks_if_has_any_permission()
+    public function test_allows_any_permissions_checking()
     {
         $readPosts = Permission::create(['name' => 'read:posts']);
         $writePosts = Permission::create(['name' => 'write:posts']);
