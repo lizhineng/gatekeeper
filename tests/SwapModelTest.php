7a63d37e15aefb0a1f5beea36229e247ea961fce
Li Zhineng <im@zhineng.li>
2023-05-15 14:37:58 +0800

    rename to swap model test

--- tests/SwapModelTest.php
+++ tests/SwapModelTest.php
@@ -5,17 +5,23 @@ namespace Zhineng\Gatekeeper\Tests;
 use Zhineng\Gatekeeper\Tests\Fixtures\MyPermission;
 use Zhineng\Gatekeeper\Tests\Fixtures\MyRole;
 
-class ManagerTest extends FeatureTest
+class SwapModelTest extends FeatureTest
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->gatekeeper->permissionUsing(MyPermission::class);
+        $this->gatekeeper->roleUsing(MyRole::class);
+    }
+
     public function test_swaps_permission_model()
     {
-        $this->gatekeeper->permissionUsing(MyPermission::class);
         $this->assertSame(MyPermission::class, $this->gatekeeper->permissionModel());
     }
 
     public function test_swaps_role_model()
     {
-        $this->gatekeeper->roleUsing(MyRole::class);
         $this->assertSame(MyRole::class, $this->gatekeeper->roleModel());
     }
 }
